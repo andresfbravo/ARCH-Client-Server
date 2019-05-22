5c34dbfffa932d81aa83959067e342df6ac3a0fc
Bravo95 <andresfelipe.bravo@utp.edu.co>
2019-05-21 23:51:13 -0500

    servidor con proxy

---
+++ tareas/FileServerProxy/Client.py
@@ -0,0 +1,121 @@
+"""
+Created on Tue Apr 16 2019
+@author: Esteban Grisales
+"""
+import zmq
+import hashlib
+import json
+import os
+import sys
+
+sizePart = 1024*1024*10
+sizeBuf = 65536
+PORT = "8001"
+file = {}
+
+class Client:
+	def ___init__(self, ident, operation, route, filename, socket):
+		pass
+
+	def Start(self):
+		os.system("clear")
+		print("\n\n-- Welcome to UltraServer¢2019 --\n")
+		print("Use <id> <function> <route> to acces interface")
+		print("\t<id> \n\t   Please use the same id for all your consults")
+		print("\t<function> there is 2 options:\n\t   \"upload\"\n\t   \"download\"")
+		print("\t<route>\n \t\trute of the file, </home/images> \n")
+		print("\t<filename>\n \t\tthe name of the file <ejemplo.jpg> \n")
+		print("##################################\n \t> ")
+
+		if len(sys.argv) != 5:
+			print("\n-- Error --\n")
+			print("\tMust be called with a route")
+			exit()
+
+		self.ident= sys.argv[1].encode()
+		self.operation = sys.argv[2].encode()
+		self.route = sys.argv[3].encode()
+		self.filename=sys.argv[4].encode()
+		#print(filename)
+
+		context = zmq.Context()
+		self.socket = context.socket(zmq.REQ)
+		self.socket.connect("tcp://localhost:"+PORT)
+		print("Establishing connection...")
+
+		if self.operation.decode()=='upload':
+		    self.upload(self.filename,self.socket, self.ident)
+		elif operation.decode()=='download':
+			self.download(self.filename,self.socket,self.ident)
+
+	def writeBytes(self,route,info):
+		newName='new-'+route
+		print("Writing file...[{}]".format(newName))
+
+		with open(newName,"wb") as f:
+		    f.write(info)
+		print("Downloaded [{}]".format(newName))
+
+	def upload(self, filename, socket, ID):
+		with open(self.route.decode()+filename.decode(), 'rb') as f :
+			sha256 = hashlib.sha256()
+			while True:
+				file = f.read(sizeBuf)
+				if not file :
+					break
+				sha256.update(file)
+		print(sha256)
+		nombreArchivo = sha256.hexdigest().encode()
+
+		with open(self.route.decode()+self.filename.decode(), "rb") as f:
+			finished = False
+			part = 0
+			while not finished:
+				print("Uploading part {}".format(part+1))
+				f.seek(part*sizePart)
+				bt = f.read(sizePart)
+				socket.send_multipart([nombreArchivo,ID, b"upload",filename, bt])
+				#print("Received reply [%s]" % (response))
+				part+=1
+				if len(bt) < sizePart:
+					finished = True
+			response = socket.recv()
+			if response.decode()=='OK':
+				print("Uploaded successfully!")
+			else:
+				print("Error!")
+
+	def download(self,filename,socket,ID):
+		#print("Download not implemented yet!!!!")
+		socket.send_multipart([ID,b'download',filename])
+		response=socket.recv_multipart()
+		filename,info=response
+		print("write[{}]".format(filename))
+		self.writeBytes(filename.decode(),info)
+"""
+	#file[os.path.basename(path)] = sha256.hexdigest()
+    with open("info.json", "w") as info:
+        json.dump(file, info)
+        name = os.path.basename(path).encode() # Nombre del archivo
+    ans = socket.recv()
+    print(ans)
+    with open(path, 'rb') as f :
+        while True:
+            data = f.read(sizeBuf)
+            if not data :
+                break
+            socket.send_multipart([name,sha256, data])      # Send Data
+            ans = socket.recv() # The server forever Reply "OK"
+            print(ans)
+with open("info.json", "a+") as f:
+    f.seek(0)
+    data = f.read(1)
+    if not data:
+        f.write("{}")
+with open("info.json") as myfile:
+    file = json.load(myfile)
+"""
+
+if __name__ == '__main__':
+	Cliente = Client()
+	Cliente.Start()
---
+++ tareas/FileServerProxy/Server.py
@@ -0,0 +1,93 @@
+"""
+Created on Tue Apr 16 2019
+@author: Esteban Grisales
+"""
+import sys
+import zmq
+import json
+import os
+
+sizePart = 1024*1024*10  #bytes
+PORT = "8001"
+register={}
+
+class Server:
+	def Start(self):
+		os.system("clear")
+		print("\n-- Welcome to UltraServer¢2019 --\n")
+		print("\tTo initialize the server provide a folder for save the files")
+		print("\t Example: python server.py <location>\n")
+
+		if len(sys.argv) != 2:
+			print("\n-- Error --\n")
+			print("\tPlease call a folder name")
+			exit()
+
+		loc=sys.argv[1]
+		if os.path.isdir('./'+loc) == False:
+			print("This route doesn't exist ")
+			print("Making new directory /{}".format(loc))
+			os.mkdir('./'+loc)
+
+		context = zmq.Context()
+		socket = context.socket(zmq.REP)
+		socket.bind("tcp://*:"+PORT)
+		print ("\nServer is now runnig in port "+PORT)
+
+		while True:
+			print("\nListening...\n")
+			sha256, ident, message,*rest = socket.recv_multipart()
+			filename,info = rest
+			register={"id":ident.decode(),"hash":sha256.decode(),"filename":filename.decode()}
+			with open("register.json", "w") as f:
+				json.dump(register, f)
+
+			print("New request: %s" % message.decode())
+			if message.decode()=='upload':
+				self.upload(sha256, filename, info, socket, ident, loc)
+			elif message.decode()=="download":
+				filename,info=rest
+				print("File: [{}]".format(filename.decode()))
+				self.download(filename, socket, ident, loc)
+			elif message.decode()=="bye":
+				exit()
+			print("Complete!")
+		# fin start
+
+	def upload(self, sha256, filename, info, socket, ident, loc) :
+
+		#newName=loc+'/'+ident.decode()+'-'+filename.decode()
+		newName = loc+'/'+sha256.decode()
+		#print("Storing as [{}]".format(newName))
+		with open(newName,"wb") as f:
+			f.write(info)
+		print("recibed")
+		socket.send(b"OK")
+		print("Send by [{}]".format(ident.decode()))
+		print("File: [{}]".format(filename.decode()))
+
+	def download(self, filename, socket, ident, loc):
+		print(filename)
+		fl=filename[0].decode()
+		#newName="./"+folder+'/'+ident.decode()+"-"+fl
+		newName=loc+'/'+sha256.decode()
+		print("Downloading [{}]".format(newName))
+		print("Send by [{}]".format(ident.decode()))
+		with open(newName, "rb") as f:
+			finished = False
+			part = 0
+			while not finished:
+				print("Uploading part {}".format(part+1))
+				f.seek(part*partSize)
+				bt = f.read(partSize)
+				socket.send_multipart([fl.encode(), bt])
+				part = part + 1
+			if len(bt) < partSize:
+				finished = True
+
+		print("Downloaded!!")
+
+
+if __name__ == '__main__':
+	Server = Server()
+	Server.Start()
