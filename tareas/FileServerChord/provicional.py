c8baccb610b64ab7b6762ca69092fe200e8c6916
bravo95 <andresfelipe.bravo@utp.edu.co>
2019-07-02 17:49:19 -0500

    modificaciones a provisional node

--- tareas/FileServerChord/provicional.py
+++ tareas/FileServerChord/provicional.py
@@ -1,9 +1,22 @@
 def download(self):
+	
+	with open(newName, "rb") as f:			
+		print("Downloading part {}".format(filename))
+		bt = f.read(sizePart)
+		print("prueba1")
+		socket.send_multipart([bt])
+		print("prueba2")
+	res = socket.recv()
+	if (res == "OK"):
+		print("send successfully!!")
+		
+	#socket.send(b"OK")
+	print("Downloaded!!")
 	pass
 
 def upload(self):
-			with open(newName,"xb") as f:
-			f.write(file)
-			self.f_space=self.f_space-1
-		socket.send(b"OK")  
-	pass
+		sha256 = self.response[1]
+		newName = self.loc+'/'+sha256.decode()
+		with open(newName,"xb") as f:
+			f.write(response[2])
+		#socket.send(b"OK")
