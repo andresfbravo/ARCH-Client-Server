3f6f075c9edba0630b26e40902030eb297386c8d
bravo95 <andresfelipe.bravo@utp.edu.co>
2019-07-01 22:28:06 -0500

    creacion del cliente

---
+++ tareas/FileServerChord/Client.py
@@ -0,0 +1,16 @@
+def upload_part(self,direction,part,hash_part):
+	#hace upload de la parte directamente en el servidor que si es 
+	pass
+
+def upload(self, ,archivo):
+	#crea .json con hash de archivo, hash de archivo como llave.
+	#parte el archivo y pone cada uno de los hash en un .json
+	#hace find de el servidor donde tiene que ir
+	#ejecuta upload_part con la direccion entregada por find
+	pass
+
+def find(self,direction):
+	#hace busqueda a partir de la direccion, cuando tiene un yes entrega la direccion para hacer upload o download
+	pass
+
+def download(self,hash_):
